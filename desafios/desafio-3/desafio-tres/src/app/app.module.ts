783de34584d065e810232474b39323611d28727e
Alexis Arce <arce.alexis@hotmail.com>
2021-11-30 12:10:49 -0300

    Add navbar component

--- desafios/desafio-3/desafio-tres/src/app/app.module.ts
+++ desafios/desafio-3/desafio-tres/src/app/app.module.ts
@@ -4,9 +4,10 @@ import { ReactiveFormsModule } from '@angular/forms';
 
 import { AppComponent } from './app.component';
 import { FormularioComponent } from './components/formulario/formulario.component';
+import { NavbarComponent } from './components/navbar/navbar.component';
 
 @NgModule({
-  declarations: [AppComponent, FormularioComponent],
+  declarations: [AppComponent, FormularioComponent, NavbarComponent],
   imports: [BrowserModule, ReactiveFormsModule],
   providers: [],
   bootstrap: [AppComponent],
