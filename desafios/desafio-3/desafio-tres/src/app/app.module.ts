9ce37920594a281ca2a05e85087aea94506725ed
Alexis Arce <arce.alexis@hotmail.com>
2021-11-30 13:38:44 -0300

    Add validations in form component

--- desafios/desafio-3/desafio-tres/src/app/app.module.ts
+++ desafios/desafio-3/desafio-tres/src/app/app.module.ts
@@ -1,6 +1,7 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { ReactiveFormsModule } from '@angular/forms';
+import { RxReactiveFormsModule } from '@rxweb/reactive-form-validators';
 
 import { AppComponent } from './app.component';
 import { FormularioComponent } from './components/formulario/formulario.component';
@@ -8,7 +9,7 @@ import { NavbarComponent } from './components/navbar/navbar.component';
 
 @NgModule({
   declarations: [AppComponent, FormularioComponent, NavbarComponent],
-  imports: [BrowserModule, ReactiveFormsModule],
+  imports: [BrowserModule, ReactiveFormsModule, RxReactiveFormsModule],
   providers: [],
   bootstrap: [AppComponent],
 })
--- desafios/desafio-3/desafio-tres/src/app/components/formulario/formulario.component.ts
+++ desafios/desafio-3/desafio-tres/src/app/components/formulario/formulario.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { FormControl, FormGroup, Validators } from '@angular/forms';
-
+import { RxwebValidators } from '@rxweb/reactive-form-validators';
+import Swal from 'sweetalert2';
 @Component({
   selector: 'app-formulario',
   templateUrl: './formulario.component.html',
@@ -12,12 +13,26 @@ export class FormularioComponent {
       Validators.required,
       Validators.minLength(3),
     ]),
-    price: new FormControl(null, [Validators.required, Validators.min(0)]),
+    price: new FormControl(null, [
+      Validators.required,
+      Validators.min(0),
+      RxwebValidators.numeric({ allowDecimal: true, isFormat: true }),
+    ]),
     description: new FormControl(null),
-    thumbnail: new FormControl(null),
+    thumbnail: new FormControl(null, [
+      Validators.pattern(
+        '(https?://)?([\\da-z.-]+)\\.([a-z.]{2,6})[/\\w .-]*/?'
+      ),
+    ]),
   });
+
+  get p() {
+    return this.productForm.controls;
+  }
 
   onSubmit() {
     console.log(this.productForm.value);
+    Swal.fire('Registro exitoso', 'Datos enviados correctamente', 'success');
+    this.productForm.reset();
   }
 }
