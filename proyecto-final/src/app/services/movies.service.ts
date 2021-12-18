c9b674b17d197ab54146874ed03debe49b84be43
aarce <Arce.Alexis@hotmail.com>
2021-12-18 00:45:00 -0300

    Use themoviedb.org API

--- proyecto-final/src/app/services/movies.service.ts
+++ proyecto-final/src/app/services/movies.service.ts
@@ -9,7 +9,11 @@ export class MoviesService {
 
   getAll() {
     return this._http.get(
-      'https://api.themoviedb.org/3/discover/movie?sort_by=popularity.desc&with_genres=35&certification_country=US&api_key=bd8cce21a42c20001a84c31a90e6b26b'
+      'https://api.themoviedb.org/3/discover/movie?sort_by=popularity.desc&with_genres=35&certification_country=US&api_key=bd8cce21a42c20001a84c31a90e6b26b&language=es-ES'
     );
+    //detalle de una película
+    //https://api.themoviedb.org/3/movie/550?api_key=bd8cce21a42c20001a84c31a90e6b26b&language=es-ES
+    //Obtiene generos, con el id se puede filtrar en la query discover (with_genres)
+    //https://api.themoviedb.org/3/genre/movie/list?api_key=bd8cce21a42c20001a84c31a90e6b26b&language=es-ES
   }
 }
